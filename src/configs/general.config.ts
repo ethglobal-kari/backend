65ba14408df6a0684d7e09dbe345b0c7d8f4b5d6
apemon <760108+apemon@users.noreply.github.com>
2022-09-19 14:24:56 +0700

    add kovan testnet

--- src/configs/general.config.ts
+++ src/configs/general.config.ts
@@ -2,10 +2,12 @@ export const generalConfig = () => ({
     eth: {
         privateKey: process.env.PRIVATE_KEY,
         network: {
-            1: process.env.ETH_RPC_URL
+            1: process.env.ETH_RPC_URL,
+            42: process.env.KOVAN_RPC_URL
         },
         factory: {
-            1: process.env.ETH_FACTORY_ADDRESS
+            1: process.env.ETH_FACTORY_ADDRESS,
+            42: process.env.KOVAN_FACTORY_ADDRESS
         }
     }
 })
