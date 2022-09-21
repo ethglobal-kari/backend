12376bd8970854890adb4ce3f382f645fa590cb9
apemon <760108+apemon@users.noreply.github.com>
2022-09-21 14:31:29 +0700

    add other network (polygon&op)

--- src/configs/general.config.ts
+++ src/configs/general.config.ts
@@ -3,11 +3,15 @@ export const generalConfig = () => ({
         privateKey: process.env.PRIVATE_KEY,
         network: {
             1: process.env.ETH_RPC_URL,
-            42: process.env.KOVAN_RPC_URL
+            42: process.env.KOVAN_RPC_URL,
+            420: process.env.OP_GOERLI_RPC_URL,
+            80001: process.env.MUMBAI_RPC_URL
         },
         factory: {
             1: process.env.ETH_FACTORY_ADDRESS,
-            42: process.env.KOVAN_FACTORY_ADDRESS
+            42: process.env.KOVAN_FACTORY_ADDRESS,
+            420: process.env.OP_GOERLI_FACTORY_ADDRESS,
+            80001: process.env.MUMBAI_FACTORY_ADDRESS
         }
     }
 })
